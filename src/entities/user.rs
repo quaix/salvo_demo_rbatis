788cbfefe8742f686f0825eb2d07f439e7bd7bd7
smallverse <zhaijunfeng2012@gmail.com>
2024-01-20 20:45:14 +0800

    init

--- src/entities/user.rs
+++ src/entities/user.rs
@@ -1,9 +1,12 @@
 use rbatis::crud;
+
+pub const TABLE_USERS: &'static str = "users";
+
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
-pub struct Users{
+pub struct Users {
     pub id: String,
     pub username: String,
     pub password: String,
 }
 
-crud!(Users {});
+crud!(Users {},TABLE_USERS);
