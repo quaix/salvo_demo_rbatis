a25afb0ba19888062b8c821e98f04b0e67b3e10b
smallverse <zhaijunfeng2012@gmail.com>
2024-01-22 14:20:01 +0800

    init

--- src/entities/user.rs
+++ src/entities/user.rs
@@ -13,13 +13,13 @@ crud!(Users {},TABLE_USERS);
 
 impl_select_page!(Users{select_page() =>"
      if !sql.contains('count(1)'):
-       `order by create_time desc`"});
+       `order by create_time desc`"},TABLE_USERS);
 
 impl_select_page!(Users{select_page_by_username(username:&str) =>"
      if username != null && username != '':
-       `where username = #{username}`"});
+       `where username = #{username}`"},TABLE_USERS);
 
 
 
 /// postgres/mssql database not support `limit 0,10`,you should use limit_sql:&str and set `limit 10 offset 0`
-impl_select_page!(Users{select_page_by_limit(username:&str,limit_sql:&str) => "`where username = #{username}`"});
+impl_select_page!(Users{select_page_by_limit(username:&str,limit_sql:&str) => "`where username = #{username}`"},TABLE_USERS);
